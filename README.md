ab6ecd3177c48abdfb0f9fe6bd69b1b7a8fa4abd
blue cushion <kwym.kwym20061211@gmail.com>
2026-06-15 23:12:36 +0900

    fix mixer weakness

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # wide-bit-blend
+
+## テスト用コマンド
+
+```
+./compiled/prng_stdout | /home/user/dev/PractRand/RNG_test stdin -tlmax 64M 2>&1
+```
+
+```
+./compiled/prng_stdout | /home/user/dev/PractRand/RNG_test stdin -tlmax 1G 2>&1
+```
--- src/include/murmur_based_2026_06_15.h
+++ src/include/murmur_based_2026_06_15.h
@@ -21,6 +21,19 @@ static void mixer_carried_murmur_2026_06_15(mt_block_t *value_ref)
         tmp.word[(i + 2) % 4] ^= tmp.word[(i) % 4];
         tmp.word[(i + 3) % 4] ^= tmp.word[(i) % 4];
     }
+    for (int i = 0; i < 4; i++)
+    {
+        // murmurハッシュ風
+        tmp.word[(i) % 4] ^= tmp.word[(i) % 4] >> 33;
+        tmp.word[(i) % 4] *= 0xff51afd7ed558ccdULL;
+        tmp.word[(i) % 4] ^= tmp.word[(i) % 4] >> 33;
+        tmp.word[(i) % 4] *= 0xc4ceb9fe1a85ec53ULL;
+        tmp.word[(i) % 4] ^= tmp.word[(i) % 4] >> 33;
+        // 上位に単純に伝播
+        tmp.word[(i + 1) % 4] ^= tmp.word[(i) % 4];
+        tmp.word[(i + 2) % 4] ^= tmp.word[(i) % 4];
+        tmp.word[(i + 3) % 4] ^= tmp.word[(i) % 4];
+    }
 
     // 返す
     *value_ref = tmp;
